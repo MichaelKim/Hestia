3b2357e55e41050657606784ff83c88260f7f3d9
Michael Kim <micklepickle98@gmail.com>
2017-02-05 10:01:06 -0500

    Send chat message on enter

--- apps/Chat/main.js
+++ apps/Chat/main.js
@@ -1,7 +1,20 @@
 app.document.getElementById("btn").onclick = function(){
+  submit();
+};
+
+app.document.getElementById("input").onkeypress = function(e){
+  if(!e) e = window.event;
+  var keyCode = e.keyCode || e.which;
+  if(keyCode === 13){
+    submit();
+    return false;
+  }
+};
+
+function submit(){
   var msg = app.document.getElementById("input").value;
   app.emit("send-msg", msg);
-};
+}
 
 app.on("new-msg", function(msg){
   var textarea = app.document.getElementById("output");
